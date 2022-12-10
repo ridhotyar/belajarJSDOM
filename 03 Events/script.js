c29f2f072376086d20b3be8948eacdb7be248daa
Ridho Tiyar <ridhotiyar@192.168.1.14>
2022-12-10 10:14:56 +0700

    Events

---
+++ 03 Events/script.js
@@ -0,0 +1,20 @@
+// const p3 = document.querySelector('#a .p3')
+
+// // pemanggilan dengan element method
+// function ubahWarna(){
+//     p3.style.backgroundColor = 'lightblue'
+// }
+
+// p3.onclick = ubahWarna
+
+// // penggunaan addeventlistener()
+
+// const p4 = document.querySelector('section#b p')
+// p4.addEventListener('click', function(){
+//     const ul = document.querySelector('section#b ul')
+//     const newli = document.createElement('li')
+//     const newliteks = document.createTextNode('Li Baru')
+//     newli.appendChild(newliteks)
+//     ul.appendChild(newli)
+// })
+
